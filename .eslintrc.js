ef553ebfc76621ed086a4d48d9339cd110b255ae
Stephen <smmstephenmiller@gmail.com>
2017-07-21 08:38:43 +0100

    Linting
    Linting configuration for eslint for following correct standard in code strucuture and coding style.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'airbnb',
+  // required to lint *.vue files
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+  globals: {}
+}
