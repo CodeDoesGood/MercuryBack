72f803c2ae09369f51c51d1e9c14b297050c089b
Stephen <smmstephenmiller@gmail.com>
2017-09-05 14:52:29 +0100

    Commenting out tests
    Commenting out the database tests for when deploying onto travis until a method of connecting to the database for travis is created that is safe while also protecting 
     the database contents.

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,1 +1,6 @@
-require('./database.test');
+/**
+ * Commenting out the database tests for when deploying onto travis until a method of
+ * connecting to the database for travis is created that is safe while also protecting
+ * the database contents.
+ */
+// require('./database.test');
