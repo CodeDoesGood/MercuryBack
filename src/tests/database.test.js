1606597628f90cf8f137d8f5457b5fbc99dd2271
Stephen <smmstephenmiller@gmail.com>
2017-09-05 15:52:59 +0100

    Removed all unused tests.
    A large amount of the tests will no longer function and are no longer revelent.

--- src/tests/database.test.js
+++ src/tests/database.test.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const assert = require('assert');
 
 const ConfigurationWrapper = require('../components/Configuration/ConfigurationWrapper');
@@ -21,97 +20,6 @@ describe('Database Wrapper', () => {
 
       const onlineStatus = databaseWrapper.getOnlineStatus();
       assert.equal(onlineStatus, false);
-    });
-  });
-
-  describe('#getProjectById', () => {
-    it('Should return project details if the id is valid', (done) => {
-      databaseWrapper.getProjectById(1).then((result) => {
-        done(assert.equal(result.id, 1));
-      }).catch(error => done(error));
-    });
-    it('Should return a empty object if the id is not a valid id', (done) => {
-      databaseWrapper.getProjectById(0).then((result) => {
-        done(assert.equal(Object.keys(result).length, 0));
-      }).catch(error => done(error));
-    });
-    it('Should return a empty object if the id is not a valid number', (done) => {
-      databaseWrapper.getProjectById('invalid').then((result) => {
-        done(assert.equal(Object.keys(result).length, 0));
-      }).catch(error => done(error));
-    });
-    it('Should return a empty object if the id is null or undefined', (done) => {
-      databaseWrapper.getProjectById(null).then((result) => {
-        done(assert.equal(Object.keys(result).length, 0));
-      }).catch(error => done(error));
-    });
-  });
-
-  describe('#GetAllProjects', () => {
-    it('Should return projects with information', (done) => {
-      databaseWrapper.getAllProjects()
-        .then((result) => {
-          assert.equal(result.length > 0, true);
-
-          _.forEach(result, (project) => {
-            assert.equal(_.isNil(project.id), false);
-          });
-
-          done();
-        }).catch(error => done(error));
-    });
-  });
-
-  describe('#getAllActiveProjects', () => {
-    it('Should return projects with active status', (done) => {
-      databaseWrapper.getAllActiveProjects()
-        .then((result) => {
-          _.forEach(result, (project) => {
-            assert.equal(project.status, 'active');
-          });
-
-          done();
-        }).catch(error => done(error));
-    });
-  });
-
-  describe('#getAllHiddenProjects', () => {
-    it('Should return projects with active status', (done) => {
-      databaseWrapper.getAllHiddenProjects()
-        .then((result) => {
-          _.forEach(result, (project) => {
-            assert.equal(project.hidden, true);
-          });
-
-          done();
-        }).catch(error => done(error));
-    });
-  });
-
-
-  describe('#getAllProjectsByStatus', () => {
-    it('Should return projects with active status passed', (done) => {
-      databaseWrapper.getAllProjectsByStatus('active')
-        .then((result) => {
-          _.forEach(result, (project) => {
-            assert.equal(project.status, 'active');
-          });
-
-          done();
-        }).catch(error => done(error));
-    });
-  });
-
-  describe('#getAllProjectsByCategory', () => {
-    it('Should return projects with correct category passed', (done) => {
-      databaseWrapper.getAllProjectsByCategory('1')
-        .then((result) => {
-          _.forEach(result, (project) => {
-            assert.equal(project.project_category, 1);
-          });
-
-          done();
-        }).catch(error => done(error));
     });
   });
 });
