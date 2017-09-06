2ce1ff43c5dcc1343fb6cbf79ab3c3af3a2be20a
Stephen <smmstephenmiller@gmail.com>
2017-09-06 19:10:22 +0100

    Database wrapper tests
    Testing the salt and hash and online status of the connection.

--- src/tests/database.test.js
+++ src/tests/database.test.js
@@ -1,16 +1,15 @@
+const _ = require('lodash');
 const assert = require('assert');
 
-const ConfigurationWrapper = require('../components/Configuration/ConfigurationWrapper');
 const DatabaseWrapper = require('../components/DatabaseWrapper/DatabaseWrapper');
-
-const config = new ConfigurationWrapper('mercury', 'mercury.json');
-const databaseWrapper = new DatabaseWrapper(config.getKey('databasePath'));
+const databaseWrapper = new DatabaseWrapper();
 
 databaseWrapper.showMessage = false;
 
 describe('Database Wrapper', () => {
   describe('#getOnlineStatus', () => {
     it('Should return true if online', () => {
+      databaseWrapper.online = true;
       const onlineStatus = databaseWrapper.getOnlineStatus();
       assert.equal(onlineStatus, true);
     });
@@ -20,6 +19,40 @@ describe('Database Wrapper', () => {
 
       const onlineStatus = databaseWrapper.getOnlineStatus();
       assert.equal(onlineStatus, false);
+    });
+  });
+
+  describe('#saltAndHash', () => {
+    const password = databaseWrapper.saltAndHash('password');
+
+    it('Should return a hashed pasword ands salt', () => {
+      assert.equal(!_.isNil(password.hashedPassword), true, 'Hashed password was not given when salting and hashing a string');
+      assert.equal(!_.isNil(password.salt), true, 'salt was not given when salting and hashing a string');
+    });
+
+    it('Should both be strings, salt and hash', () => {
+      assert.equal(_.isString(password.hashedPassword), true, 'Hashed password was not a string when salting and hashing a string');
+      assert.equal(_.isString(password.salt), true, 'salt was not a string when salting and hashing a string');
+    });
+
+    it('Should validate the salt and hash if the salt is passed', () => {
+      const compareablePassword = databaseWrapper.saltAndHash('password', password.salt);
+      assert.equal(compareablePassword.hashedPassword, password.hashedPassword, 'Password salted with stored salt do not match');
+    });
+
+    it('Should be a invalid match when entering the wrong salt', () => {
+      const compareablePassword = databaseWrapper.saltAndHash('password', `${password.salt}wrongsalt`);
+      assert.equal(compareablePassword.hashedPassword === password.hashedPassword, false, 'Password salted with wrong salt should not match');
+    });
+
+    it('Should be a invalid match when entering the wrong password but correct salt', () => {
+      const compareablePassword = databaseWrapper.saltAndHash('wrongpassword', password.salt);
+      assert.equal(compareablePassword.hashedPassword === password.hashedPassword, false, 'Password salted with wrong password should not match');
+    });
+
+    it('Should be a invalid match when entering the wrong password and wrong salt', () => {
+      const compareablePassword = databaseWrapper.saltAndHash('wrongpassword', `${password.salt}wrongsalt`);
+      assert.equal(compareablePassword.hashedPassword === password.hashedPassword, false, 'Password salted with wrong password should not match');
     });
   });
 });
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,6 +1,1 @@
-/**
- * Commenting out the database tests for when deploying onto travis until a method of
- * connecting to the database for travis is created that is safe while also protecting
- * the database contents.
- */
-// require('./database.test');
+require('./database.test');
